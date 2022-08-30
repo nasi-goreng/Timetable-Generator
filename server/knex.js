e4d7df02c44b2099c150330806ff1a2f7ca50374
riko <54302736+nasi-goreng@users.noreply.github.com>
2022-08-31 00:44:26 +0900

    tried changing how I wrote knex.js to see if it works

--- server/knex.js
+++ server/knex.js
@@ -1,14 +1,28 @@
-const knex = require("knex");
-require("dotenv").config({
-  path: "./.env.local",
-});
+// original working code
 
-const db = knex({
-  client: "pg",
-  connection:
-    process.env.DATABASE_URL ||
-    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/schedule`,
-  searchPath: "public",
-});
+// const knex = require("knex");
+// require("dotenv").config({
+//   path: "./.env.local",
+// });
+
+// const db = knex({
+//   client: "pg",
+//   connection:
+//     process.env.DATABASE_URL ||
+//     `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/schedule`,
+//   searchPath: "public",
+// });
+
+// module.exports = db;
+
+// from sprint knex
+// const config = require("../knexfile");
+// const knex = require("knex")(config);
+
+// module.exports = knex;
+
+// changed knex to db from above code 
+const config = require("../knexfile");
+const db = require("knex")(config);
 
 module.exports = db;
