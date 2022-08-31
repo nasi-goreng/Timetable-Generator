ac105c0087a9fac6727adddb24ee6b9bd38cdc56
riko <54302736+nasi-goreng@users.noreply.github.com>
2022-08-31 20:36:32 +0900

    added cells for students

--- server/knex.js
+++ server/knex.js
@@ -9,7 +9,7 @@ const db = knex({
   client: "pg",
   connection:
     process.env.DATABASE_URL ||
-    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/schedule`,
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/schedule2`,
   searchPath: "public",
 });
 
